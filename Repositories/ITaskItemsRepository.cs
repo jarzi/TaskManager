308f4cf07cab0fc75a0d6b2670974bbd82ebcf2d
Michał Jastrząb <jarzi@users.noreply.github.com>
2022-04-24 17:13:10 +0200

    Change return value

---
+++ Repositories/ITaskItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TasksWebApi.Entities;
+
+namespace TasksWebApi.Repositories
+{
+    public interface ITaskItemsRepository
+    {
+        public Task<int> CreateTaskItemAsync(TaskItem taskItem);
+        public Task<TaskItem> GetTaskItemAsync(int id);
+        public Task<IEnumerable<TaskItem>> GetTaskItemsAsync();
+        public Task<int?> UpdateTaskItemAsync(TaskItem taskItem);
+        public Task<bool> DeleteTaskItemAsync(int id);
+    }
+}
